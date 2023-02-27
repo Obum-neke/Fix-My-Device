3d425ec5d970ec5eeeb158626e3c55ed657f0004
obumneke <obumnekeeze4@gmail.com>
2023-02-28 00:20:34 +0100

    third-commit

--- src/routes/RepairmyDevice.js
+++ src/routes/RepairmyDevice.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../index.css";
 import Navbar from "../components/Navbar";
-import iphone from "../assets/iphone.png"
+// import iphone from "../assets/iphone.png"
 import android from "../assets/android.png"
 import tablet from "../assets/tablet.png"
 import laptop from "../assets/laptop.png"
@@ -15,7 +15,7 @@ const RepairmyDevice = () => {
      <Navbar />
      <h1>Please Select Your Device</h1> 
      <div>
-     <img src={iphone} alt="" />
+     {/* <img src={iphone} alt="" /> */}
         <img src={android} alt="" />
         <img src={tablet} alt="" />
         <img src={laptop} alt="" />
